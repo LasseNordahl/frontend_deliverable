99d7dd2a478a406384fa11ce090ba3c4e462dcf7
Lasse Nordahl <lasseanordahl@gmail.com>
2019-03-12 13:47:06 -0700

    post is complete for the form. Just need to add character limit to the message and the requirements are met

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import FeedbackForm from './components/FeedbackForm';
 class App extends Component {
   render() {
     return (
-      <div className="App appFlex centerFlex">
+      <div className="App application centerRowFlex">
+        {/* {[1,2,3,4,5,6,7,8,9,10].map(() => {
+          return(
+            <div style={{'width': '10px', 'height': '10px', 'backgroundColor': 'white'}}> </div>
+            )
+        })} */}
         <FeedbackForm></FeedbackForm>
       </div>
     );
--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,47 +1,60 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './../App';
 
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
+import Axios from 'axios';
+
+const FEEDBACK_URL = 'https://tranquil-lowlands-24043.herokuapp.com/feedback';
 
 class FeedbackForm extends Component {
 	constructor(props) {
 		super(props)
 
-		this.state = {
-			formData: {
-				first: '',
-				last: '',
-				email: '',
-				message: ''
-			}
-		}
+		this.state = {}
 
 		this.handleFormChange = this.handleFormChange.bind(this);
-		this.submitFeedback = this.submitFeedback.bind(this);
+		this.submitForm = this.submitForm.bind(this);
+		this.postFeedback = this.postFeedback.bind(this);
 	}
 	
 	handleFormChange(e) {
-		let formData = Object.assign({}, this.state.formData)
-		formData[e.target.name] = e.target.value
+		let formData = Object.assign({}, this.state.formData);
+		formData[e.target.name] = e.target.value;
+	
 		this.setState({
 			formData: formData
 		});
 	}
 
-	submitFeedback() {
-		console.log(this.state.formData);
-	}
-
 	submitForm(e) {
 		e.preventDefault();
-		console.log(e.target)
+
+		let formData = {
+			first: e.target.elements.first.value,
+			last: e.target.elements.last.value,
+			email: e.target.elements.email.value,
+			message: e.target.elements.message.value
+		};
+
+		this.postFeedback(formData);
+	}
+
+	postFeedback(formData) {
+		let self = this
+
+		axios.post(FEEDBACK_URL, formData)
+		.then(function(response) {
+			console.log(response);
+		})
+		.catch(function(error) {
+			console.log(error);
+		})
 	}
 
   render() {
-		const { formData } = this.state;
-
     return (
       <div className="feedbackForm">
         <h2 className="formTitle">Feedback</h2>
@@ -50,22 +63,20 @@ class FeedbackForm extends Component {
 						<Form.Label>First Name</Form.Label>
 						<Form.Control 
 								// required
-								// value={formData.first}
 								name='first'
-								// onChange={this.handleFormChange}
 								type="text"
 								placeholder="Peter" 
+								defaultValue="Peter"
 						/>
 					</Form.Group>
 					<Form.Group>
 						<Form.Label>Last Name</Form.Label>
 						<Form.Control 
-								// required
-								// value={formData.last}
-								// name='last'
-								// onChange={this.handleFormChange}
+								required
+								name='last'
 								type="text"
 								placeholder="Anteater" 
+								defaultValue="Anteater"
 						/>
 					</Form.Group>
 					<Form.Group>
@@ -76,11 +87,10 @@ class FeedbackForm extends Component {
 							</InputGroup.Prepend>
 							<Form.Control 
 									// required
-									// value={formData.email}
-									// name='email'
-									// onChange={this.handleFormChange}
+									name='email'
 									type="email"
-									placeholder="PeterAnteater@uci.edu" 
+									placeholder="peteranteater@uci.edu" 
+									defaultValue="pa@uci.edu"
 							/>
 						</InputGroup>
 					</Form.Group>
@@ -88,18 +98,16 @@ class FeedbackForm extends Component {
 						<Form.Label>Message</Form.Label>
 						<Form.Control 
 							// required
-							// value={formData.message}
-							// name='message'
-							// onChange={this.handleFormChange}
+							name='message'
 							type="text"
 							as="textarea" 
 							rows="3" 
 							maxLength={500}
 							placeholder="The boba cost more than 2 dollars :("
+							defaultValue="2 dollar bober"
 						/>
 					</Form.Group>
 					<Button 
-						// onClick={this.submitFeedback}
 						className="submitButton" 
 						variant="primary" 
 						size="lg" 
